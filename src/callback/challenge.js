112d3fb3e9b44f9aae5cc9ef08d17f0c84e09242
Oscar Barajas Tavares <oscar@gndx.dev>
2022-04-25 16:09:13 -0500

    :sparkles: [ADD] XMLHttpRequest

---
+++ src/callback/challenge.js
@@ -0,0 +1,19 @@
+const XMLHttpRequest = require('xmlhttprequest');
+const API = 'https://api.escuelajs.co/api/v1';
+
+function fetchData(urlApi, callback) {
+  let xhttp = new XMLHttpRequest();
+
+  xhttp.open('GET', urlApi, true);
+  xhttp.onreadystatechange = function (event) {
+    if (xhttp.readyState === 4) {
+      if(xhttp.status === 200) {
+        callback(null, JSON.parse(xhttp.resposeText));
+      }
+    } else {
+      const error = new Error('Error' + urlApi);
+      return callback(error, null);
+    }
+  }
+  xhttp.send();
+}
